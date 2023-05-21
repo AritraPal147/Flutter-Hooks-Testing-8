b31168a29fd955d35a0cbe2a3f7ee37026221a6f
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-21 19:36:40 +0530

    Added AppLifecycle hook -> allows us to hide sensitive information on minimization

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,40 @@ void main() {
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  /// [state] stores the AppLifecycleState which allows us to hide
+  /// sensitive information when app is minimized and snapshot of app
+  /// is shown in recent apps
   @override
   Widget build(BuildContext context) {
+    final state = useAppLifecycleState();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+      ),
+      body: Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Opacity(
+            /// Checks is current state is AppLifecycleState.resumed
+            /// If true, then opacity is set to 1.0
+            ///
+            /// If false, then app in minimized, so the sensitive information must be
+            /// hidden -> opacity is set to 0.0
+            opacity: state == AppLifecycleState.resumed ? 1.0 : 0.0,
+            child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  blurRadius: 10,
+                  color: Colors.black.withAlpha(100),
+                  spreadRadius: 10,
+                )
+              ]),
+              child: Image.asset('assets/card.png'),
+            ),
+          ),
+        ),
       ),
     );
   }
